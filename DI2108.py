9653e2851175bddd56a080f33b64844a8a8b4c57
Troy Denton <trdenton@gmail.com>
2017-03-04 00:46:11 -0600

    added, commented commands

--- DI2108.py
+++ DI2108.py
@@ -2,6 +2,7 @@
 import usb
 import time
 
+
 '''
 Python class for reading DI2108 sensor from porcupine labs
 '''
@@ -18,7 +19,7 @@ class DI2108(object):
   ENDPOINT_OUT=0x01
 
   '''
-  :returns: array of usb devices corresponding to the DI2108 from porcupine labs
+  :returns: array of usb devices corresponding to the DI2108
   '''
   @staticmethod
   def _getDevices():
@@ -86,20 +87,191 @@ class DI2108(object):
     out_string = " ".join(args) + "\r"
     self._write_cmd_string(out_string)
 
-
+  #
+  # User commands
+  #
+  #
 
   def info(self,arg0):
+    """Get info about the DI2108.
+
+    arguments:
+    arg0 -- an integer specifying data to retrieve:
+      0 - returns "DATAQ"
+      1 - Returns device name: "2108"
+      2 - Returns firmware revision, 2 hex bytes (e.g. 6516 = 10110 for firmware revision 1.01)
+      3 - Proprietary internal use for initial system verification
+      4 - Proprietary internal use for initial system verification
+      5 - Proprietary internal use for initial system verification
+      6 - info 6 Returns the DI-2108's serial number (left-most 8 digits only; right-most two digital are for internal use)
+      7 - Proprietary internal use for initial system verification
+      8 - Proprietary internal use for initial system verification
+      9 - Returns the sample rate divisor value of 60,000,000 for the DI-2108 (see the srate command for details)
+
+    returns an echo
+    """
     self._write_cmd_args(['info',str(arg0)]) 
     return self._read()
 
   def ps(self,arg0):
-    self._write_cmd_args(['ps'],str(arg0))
+    """Set packet size
+
+    arguments:
+    arg0 -- aan integer specifying packet size:
+      0 - Make packet size 16 bytes
+      1 - Make packet size 32 bytes
+      2 - Make packet size 64 bytes
+      3 - Make packet size 128 bytes
+      4 - Make packet size 256 bytes
+      5 - Make packet size 512 bytes
+      6 - Make packet size 1024 bytes
+      7 - Make packet size 2048 bytes
+
+    returns an echo
+    """
+    self._write_cmd_args(['ps',str(arg0)])
     return self._read()
 
+  def start(self,arg0):
+    """Initiates scanning (reading inputs)
 
-  
-   
+    arguments:
+    arg0 -- bleh
+      0 - Normal scanning: The instrument begins scanning the channels enabled in its scan list through the slist command at a rate defined by the srate command.
+      1 - Reserved for future use. 
+      2 - Scan using an external clock or trigger: The instrument begins scanning the channels enabled in its scan list by
+          the slist command at a rate defined by clock transitions applied to its "Ext Trig" input of D6. This scan method
+          allows data to be acquired synchronously with external events.
+      3 - Immediate scan: The instrument reports the results of a single scan of all channels enabled in its scan list at a
+          burst rate of 1 kHz per channel. Analog channels are reported at fixed 16-bit resolution. 
+
+    returns nothing.
+    """
+    self._write_cmd_args(['start',str(arg0)])
+    return  #never echoes
+
+  def stop(self):
+    """Terminates scanning.
+    returns an echo
+    """
+    self._write_cmd_args(['stop'])
+    return  self._read()
  
+  def slist(self,arg0,arg1):
+    """Used to configure the scan list.
+
+    parameters:
+    arg0 -- offset
+    arg1 -- config
+
+    returns an echo
+    """
+    self._write_cmd_args(['slist',str(arg0),str(arg1)])
+    return  self._read()
+ 
+  def srate(self,arg0):
+    """defines a sample rate divisor used to determine scan rate, or the rate at which the DI-
+    2108 scans through the items in the scan list that you defined with the slist command
+
+    Sample rate per scan list element (Hz) = 60,000,000 ÷ (srate × dec)
+
+    parameters:
+    arg0 -- the srate to set
+
+    returns an echo 
+    """
+    self._write_cmd_args(['srate',str(arg0)])
+    return  self._read()
+
+  def filter(self,arg0,arg1):
+    """ Changes the acquisition mode for an analog channel.
+
+    parameters:
+    arg0 -- analog channel number
+    arg1 -- acquisition mode:
+      0 - Last Point
+      1 - CIC filter
+      2 - Maximum
+      3 - Minimum
+
+    returns an echo 
+    """
+    self._write_cmd_args(['filter',str(arg0),str(arg1)])
+    return  self._read()
+ 
+  def dec(self,arg0):
+    """sets the number of samples used to calculate the CIC filter
+
+    parameters:
+    arg0 -- 1 ≤ arg0 ≤ 512 sets the number of values used by the Acquisition Mode defined by the filter
+            command.
+    """
+    self._write_cmd_args(['dec',str(arg0)])
+    return  self._read()
+
+  def ffl(self,arg0):
+    """Configure the moving average filter
+
+    parameters:
+    arg0 -- the MA factor
+
+    returns an echo
+    """
+    self._write_cmd_args(['ffl',str(arg0)])
+    return  self._read()
+
+  def led(self,arg0):
+    """Turn on the LED
+
+    parameters:
+    arg0 -- the LED color:
+      0 - black
+      1 - blue
+      2 - green
+      3 - cyan
+      4 - red
+      5 - magenta
+      6 - yellow
+      7 - white
+
+    returns an echo
+    """
+    self._write_cmd_args(['led',str(arg0)])
+    return  self._read()
+
+  def dout(self,arg0):
+    self._write_cmd_args(['dout',str(arg0)])
+    return  self._read()
+
+  def endo(self,arg0):
+    """defines configuration on a per port basis, input or switch.
+
+    parameters:
+    arg0 -- binary configuration; 0 ≤ arg0 ≤ 12710 and maps input/switch configuration to each of seven digital ports. A value
+            of one written to a port configures it as a switch. A value of zero configures the port as an
+            input. 
+
+    returns an echo
+    """
+    self._write_cmd_args(['endo',str(arg0)])
+    return  self._read()
+
+  def din(self):
+    """returns the state of all ports as a 7-bit value.
+
+    returns din <status>
+    """
+    self._write_cmd_args(['din'])
+    return  self._read()
+
+  def reset(self):
+    """reset the DI-2108 counter
+    returns an echo
+    """
+    self._write_cmd_args(['reset 1'])
+    return  self._read()
+
+
 if __name__=="__main__":
   d= DI2108.listDevices()		
 
