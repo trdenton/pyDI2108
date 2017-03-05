88a61b7fdb5bd593a76096a1d8e88bdb3f4b6b1e
Troy Denton <trdenton@gmail.com>
2017-03-05 08:56:04 -0600

    now doing some very basic reading

--- DI2108.py
+++ DI2108.py
@@ -18,17 +18,67 @@ class DI2108(object):
   ENDPOINT_IN=0x81
   ENDPOINT_OUT=0x01
 
+
+  PACKET_SIZE_16=0
+  PACKET_SIZE_32=1
+  PACKET_SIZE_64=2
+  PACKET_SIZE_128=3
+  PACKET_SIZE_256=4
+  PACKET_SIZE_512=5
+  PACKET_SIZE_1024=6
+  PACKET_SIZE_2048=7
+
+  PACKET_SIZE_ARG_TO_SIZE=[  16,  32,  64,  128,  256,  512,  1024,  2048  ]
+
+  SCAN_MODE_NORMAL=0
+  SCAN_MODE_EXTERNAL=2
+  SCAN_MODE_IMMEDIATE=3
+
+  CHANNEL_ANALOG_0=0
+  CHANNEL_ANALOG_1=1
+  CHANNEL_ANALOG_2=2
+  CHANNEL_ANALOG_3=3
+  CHANNEL_ANALOG_4=4
+  CHANNEL_ANALOG_5=5
+  CHANNEL_ANALOG_6=6
+  CHANNEL_ANALOG_7=7
+  CHANNEL_DIGITAL_IN=8
+  CHANNEL_DIGITAL_RATE=9
+  CHANNEL_DIGITAL_COUNT=10
+  CHANNEL_IGNORE=0xFF
+  CHANNEL_DIGITAL_RATE_RANGE_50000HZ=(1<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_20000HZ=(2<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_10000HZ=(3<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_5000HZ=(4<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_2000HZ=(5<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_1000HZ=(6<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_500HZ=(7<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_200HZ=(8<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_100HZ=(9<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_50HZ=(10<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_20HZ=(11<<8)
+  CHANNEL_DIGITAL_RATE_RANGE_10HZ=(12<<8)
+
+  LED_OFF=0
+  LED_BLUE=1
+  LED_GREEN=2
+  LED_CYAN=3
+  LED_RED=4
+  LED_MAGENTA=5
+  LED_YELLOW=6
+  LED_WHITE=7
+
   '''
   :returns: array of usb devices corresponding to the DI2108
   '''
   @staticmethod
-  def _getDevices():
+  def _get_devices():
     return map(DI2108,list(usb.core.find(find_all=True,idVendor=DI2108.ID_VENDOR, idProduct=DI2108.ID_PRODUCT)))
 
   
   @staticmethod
-  def listDevices():
-    return DI2108._getDevices()
+  def list_devices():
+    return DI2108._get_devices()
 
 
   '''
@@ -36,28 +86,30 @@ class DI2108(object):
   :param dev: usb.core.Device instance corresponding to the DI2108
   '''
   def __init__(self,dev):
-    self.usbDevice=dev
-    self.usbDevice.reset()
-    if(self.usbDevice.is_kernel_driver_active(DI2108.INTERFACE_NUM)):
-      self.usbDevice.detach_kernel_driver(DI2108.INTERFACE_NUM)
+    self.last_reading=None
+    self.usb_device=dev
+    self.usb_device.reset()
+    if(self.usb_device.is_kernel_driver_active(DI2108.INTERFACE_NUM)):
+      self.usb_device.detach_kernel_driver(DI2108.INTERFACE_NUM)
+    self.set_packet_size(DI2108.PACKET_SIZE_64)
 
   '''
   Close the device
   '''
   def close(self):
-    self.usbDevice.reset()
-    return self.usbDevice.attach_kernel_driver(DI2108.INTERFACE_NUM)
+    self.usb_device.reset()
+    return self.usb_device.attach_kernel_driver(DI2108.INTERFACE_NUM)
 
   '''
   Read a byte from the DI2108
   '''
   def _read(self):
-    size=128
+    size=self.packet_size
     output=bytearray()
-    x=self.usbDevice.read(DI2108.ENDPOINT_IN,size,timeout=400)
+    x=self.usb_device.read(DI2108.ENDPOINT_IN,size,timeout=400)
     output.extend(x)
     while(x[-1]!=0):
-      x=self.usbDevice.read(DI2108.ENDPOINT_IN,size,timeout=400)
+      x=self.usb_device.read(DI2108.ENDPOINT_IN,size,timeout=400)
       output.extend(x)
     return output.decode('ascii')
 
@@ -68,7 +120,7 @@ class DI2108(object):
   :param arr: array of ints to write to DI2108
   '''
   def _write(self,arr):
-    return self.usbDevice.write(DI2108.ENDPOINT_OUT,bytearray(arr))
+    return self.usb_device.write(DI2108.ENDPOINT_OUT,bytearray(arr))
 
 
   '''
@@ -88,8 +140,7 @@ class DI2108(object):
     self._write_cmd_string(out_string)
 
   #
-  # User commands
-  #
+  # DATAQ commands
   #
 
   def info(self,arg0):
@@ -136,7 +187,7 @@ class DI2108(object):
 
     returns an echo
     """
-    self._write_cmd_args(['ps',str(arg0)])
+    self._write_cmd_args(['ps',str(arg0)+' '])
     return self._read()
 
   def start(self,arg0):
@@ -162,7 +213,8 @@ class DI2108(object):
     returns an echo
     """
     self._write_cmd_args(['stop'])
-    return  self._read()
+    while self.read_data():
+      pass
  
   def slist(self,arg0,arg1):
     """Used to configure the scan list.
@@ -279,8 +331,101 @@ class DI2108(object):
     return  self._read()
 
 
+  #
+  # User interface commands
+  #
+
+  def check_connection(self):
+    return "DATAQ"==self.info(0)
+
+  def get_device_name(self):
+    """returns the device name "2108"
+    """
+    return self.info(1)
+
+  def get_firmware_revision(self):
+    """returns firmware revision, 2 hex bytes
+    """
+    return self.info(2)
+
+  def get_serial_number(self):
+    """returns erial number (left-most 8 digits only; right-most two digits are for internal use)
+    """
+    return self.info(6)
+
+  def get_sample_rate_divisor(self):
+    """returns the sample rate divisor value (60,000,000 for the DI-2108)
+    """
+    return int(self.info(9))
+
+  def set_packet_size(self,arg):
+    """sets the packet size
+    arg - one of PACKET_SIZE_16, PACKET_SIZE_32, PACKET_SIZE_64, PACKET_SIZE_128, PACKET_SIZE_256, 
+          PACKET_SIZE_512, PACKET_SIZE_1024, PACKET_SIZE_2048
+    """
+    self.packet_size=DI2108.PACKET_SIZE_ARG_TO_SIZE[arg]
+    self.ps(arg)
+
+  def get_packet_size(self):
+    """returns the packet size- one of PACKET_SIZE_16, PACKET_SIZE_32, PACKET_SIZE_64, PACKET_SIZE_128, 
+        PACKET_SIZE_256, PACKET_SIZE_512, PACKET_SIZE_1024, PACKET_SIZE_2048
+    """
+    return self.packet_size
+
+
+  def start_reading(self,arg):
+    """Start reading data
+    arg -- one of SCAN_MODE_NORMAL, SCAN_MODE_EXTERNAL, SCAN_MODE_IMMEDIATE
+    """
+    self.scan(arg)
+
+  def stop_reading(self,arg):
+    """Stop reading data
+    """
+    self.stop()
+
+  def add_channel_to_list(self,pos,ch):
+    """Add channel to scan list
+    pos -- the position to add ch to  in the scan list (0-11)
+    ch -- the channel to use (CHANNEL_ANALOG_0, etc)
+    """
+    self.slist(ch,pos)
+
+  def read_data(self):
+    try:
+      x=self.usb_device.read(DI2108.ENDPOINT_IN,self.packet_size,timeout=1)
+      self.last_reading=x
+      return True
+    except Exception as e: #data timed out
+      return False
+
+  def get_channel(self,channel_num):
+    #split up every 2 bytes
+    if self.last_reading != None:
+      byte_1 = self.last_reading[channel_num*2]
+      byte_2 = self.last_reading[channel_num*2+1]
+      return (byte_2<<8)|(byte_1)
+    else:
+      return None
+
+
+  def get_analog_channel(self,channel_num):
+    data=self.get_channel(channel_num)
+    print "Raw data: %x"%data
+    if data != None:
+      #signed, 16-bit twos complement
+      if (data & (1<<15)) != 0:
+        data = data-(1<<16)
+      volts = 10.0*data/32768.0
+      return volts
+    else:
+      return None
+      
+      
+    
+
 if __name__=="__main__":
-  d= DI2108.listDevices()		
+  d= DI2108.list_devices()		
 
   for x in xrange(1,10):
     print d[0].info(x)
--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import time
 from DI2108 import DI2108
 
-ds= DI2108.listDevices()		
+ds= DI2108.list_devices()		
 
 #get first device
 d = ds[0]
@@ -17,7 +17,36 @@ print d.info(2)
 print d.info(6)
 print d.info(9)
 
+d.set_packet_size(DI2108.PACKET_SIZE_2048)
+d.set_packet_size(DI2108.PACKET_SIZE_1024)
+d.set_packet_size(DI2108.PACKET_SIZE_512)
+d.set_packet_size(DI2108.PACKET_SIZE_256)
+d.set_packet_size(DI2108.PACKET_SIZE_128)
+d.set_packet_size(DI2108.PACKET_SIZE_64)
+d.set_packet_size(DI2108.PACKET_SIZE_32)
+d.set_packet_size(DI2108.PACKET_SIZE_16)
+
 #test LED commands
 for x in xrange(0,8):
   d.led(x)
   time.sleep(0.2)
+
+
+#try reading some data
+d.led(DI2108.LED_GREEN)
+d.add_channel_to_list(0,DI2108.CHANNEL_ANALOG_0)
+d.set_packet_size(DI2108.PACKET_SIZE_2048)
+print "Packet size...%d"%d.get_packet_size()
+raw_input("Hit enter to begin")
+d.start(DI2108.SCAN_MODE_NORMAL)
+
+try:
+  while True:
+    if d.read_data():
+      reading = d.get_analog_channel(0)
+      if reading != None:
+        print "%f"%reading
+except KeyboardInterrupt as ki:
+  d.stop()
+
+d.led(DI2108.LED_WHITE)
